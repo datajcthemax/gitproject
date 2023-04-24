1f5784456caa0d2d5dad1c4adeb3af92bd78605a
yaerimkim <fairy0yeri@naver.com>
2023-04-24 17:47:51 +0900

    preprocessing

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 train_data = pd.read_csv('C:\\workspace\\gitproject\\datas\\train.csv')
-test_data = pd.read_csv('C:\\workspace\\gitproject\datas\\test.csv')
+test_data = pd.read_csv('C:\\workspace\\gitproject\\datas\\test.csv')
 
 # 전처리
 train_data.isna().sum()
@@ -31,12 +31,10 @@ X_train.scaled = scaler.fit_transform(X_train)
 X_test.scaled = scaler.transform(X_test)
 
 X_train_scaled = train_data[['Exercise_Duration','Body_Temperature(F)','BPM','Height(Feet)','Weight(lb)','Age','Gender_F','Gender_M','Weight_Status_Normal Weight','Weight_Status_Obese','Weight_Status_Overweight']]
-X_test_scaled = test_data[['Exercise_Duration','Body_Temperature(F)','BPM','Height(Feet)','Weight(lb)','Age','Gender_F','Gender_M','Weight_Status_Normal Weight','Weight_Status_Obese','Weight_Status_Overweight']]
-
 X_train_scaled['Calories_Burned'] = train_data['Calories_Burned']
 
-print(X_test_scaled)
+print(X_test)
 print(X_train_scaled)
 
 X_train_scaled.to_csv('2_train.csv', index=False)
-X_test_scaled.to_csv('2_test.csv',index=False)
+X_test.to_csv('2_test.csv',index=False)
